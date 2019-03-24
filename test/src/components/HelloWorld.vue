b32e0fce33f21cf529419a7e10713c2a5d02f0db
ifdwy <1193044859@qq.com>
2019-03-24 23:25:53 +0800

    用vue-cli写的第一个测试界面

---
+++ test/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id=''>
+    <a href="#" class="list-group-item active">
+      <div class="pull-left">dsds</div>
+    </a>
+    <a href="#" class="list-group-item">24*7 支持</a>
+    <a href="#" class="list-group-item">免费 Window 空间托管</a>
+    <a href="#" class="list-group-item">图像的数量</a>
+    <a href="#" class="list-group-item">每年更新成本</a>
+  </div>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+export default {
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
